ee5efc01e03cdaace194047951df623a11e6a79f
Vivekthakur24 <151036562+Vivekthakur24@users.noreply.github.com>
2024-11-04 13:00:11 +0530

    Add files via upload

---
+++ 02_datatype.js
@@ -0,0 +1,31 @@
+"use strict ";  // treate all JS  code newer version 
+//alert (3+3)    // we are using nodejs , not browser
+
+
+
+
+let name = " vivek "    // string data type 
+let age = 18
+let isLoggedIn = false
+
+
+//  range of data type 
+/*
+number = 2 to the power 53
+big int = 
+string => "" ,''
+boolean => true/false
+null => standalone value
+undefine   =>   means value not define of the data type
+symbol =>  to  denote the unique nees of the value
+
+
+object  
+*/
+console.log(typeof undefined)//  output will be undefine 
+console.log(typeof null) // output will be  object
+console.table([name,age ])
+
+
+
+
---
+++ 03_datatype_and_operation.js
@@ -0,0 +1,37 @@
+ //"33"=> 33
+ // 33a => Nan 
+ // true => 1   and  false => 0
+
+
+//   let isLoggedIn  =1
+//   let  booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn)
+
+  // 1 => true 
+  // "" =>  false 
+  // "vivek " => true
+
+
+
+
+//   let someNumber =33
+
+//   let stringNumber = String(someNumber)
+//   console.log(  typeof stringNumber)
+
+
+  //*************************************************some important operation ******************************************************** */
+// operation 1 
+  let value = 3
+  let negValue = -value
+  console.log(negValue)
+
+  //operation (some basic mathematical operation)
+
+ // console.log(2+2)    // addition
+//   console.log(2-2)     subtration   
+//   console.log(2**2)   squaring
+//   console.log(2/2)      dividing
+//   console.log(2%2)     remainder
+
+
